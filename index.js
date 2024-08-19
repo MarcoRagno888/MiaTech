0089d19bced706de57aabc24d460f2264b8a9b8b
MarcoRagno888 <marco.cianci@5gmail.com>
2024-08-19 14:42:04 +0200

    feat: cambio di valore

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-let first;
+/*let first;
 const second = 2;
 
 first = 1;
 
-console.log(first, second);
+console.log(first, second);*/
+
+let change = 5;
+
+console.log(change);
+
+change = 1;
+
+console.log(change);
