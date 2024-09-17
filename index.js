d72c4e7d149cc7bfaa6aebcc77771bbb3e684751
MarcoRagno888 <marco.cianci@5gmail.com>
2024-09-17 15:20:20 +0200

    feat: changed style

--- index.js
+++ index.js
@@ -1100,3 +1100,5 @@ const $paragraph1 = document.querySelector("p");
 const $paragraph2 = document.getElementById("innerT");
 
 $paragraph2.innerText = "Testo aggiunto con Javascript";
+$paragraph2.style.backgroundColor = "red";
+$paragraph2.style.fontSize = "20px";
