ef07a7f7daff8fae77be8d367fa5eff3bae64a1c
MarcoRagno888 <marco.cianci@5gmail.com>
2024-10-11 13:12:40 +0200

    feat: input ref 3

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -3,12 +3,18 @@ import HelloWorld from './HelloWorld';
 
 const UncontrolledInput = () => {
   const inputRef = useRef(null);
+
+  const handleClick = () => {
+    alert(inputRef.current.value);
+  }
   
   return (
     <>
       <div className="p-4">
         <label htmlFor="input">Input Ref: </label>
         <input type="text" ref={inputRef} name="input" className="border border black" />
+
+        <button type="submit" onClick={handleClick} className="btn">Submit</button>
       </div>
     </>
   )
