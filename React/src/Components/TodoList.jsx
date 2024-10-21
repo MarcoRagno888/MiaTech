8bdec19e89f263c6b88f7c7cc8a955ec25ebc202
MarcoRagno888 <marco.cianci@5gmail.com>
2024-10-21 16:11:58 +0200

    feat: useFilteredTodos in TodoList

--- React/src/Components/TodoList.jsx
+++ React/src/Components/TodoList.jsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import { useFetch } from "../hooks/useFetch";
+import { useFilteredTodos } from "../hooks/useFilteredTodos";
 
 const API_URL = "https://jsonplaceholder.typicode.com/todos";
 
 const TodoList = () => {
     const { data, loading, error } = useFetch(API_URL);
+
+    const [selector, setSelector] = useState("");
+    const filtered = useFilteredTodos(data, selector);
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error: {error}</p>
@@ -12,6 +17,26 @@ const TodoList = () => {
         <>
             <div className="p-2">
                 <h1>To Do List</h1>
+                <div>
+                    <input
+                        type="text"
+                        className="border border-black"
+                        placeholder="Cerca to do..."
+                        value={selector}
+                        onChange={(e) => setSelector(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <ul>
+                        {filtered.length > 0 ? (
+                            filtered.map(todo => (
+                                <li key={todo.id}>{todo.title}</li>
+                            ))
+                        ) : (
+                            <li>Nessun to-do trovato</li>
+                        )}
+                    </ul>
+                </div>
                 <ul>
                     {
                         data && data.map((todo) => (
