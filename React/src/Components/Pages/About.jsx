91a76e4ac830d23b426b8f213547d3f901a57c52
MarcoRagno888 <marco.cianci@5gmail.com>
2024-10-28 17:14:19 +0100

    feat: Navigate

--- React/src/Components/Pages/About.jsx
+++ React/src/Components/Pages/About.jsx
@@ -1,7 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
 const About = () => {
+    const navigate = useNavigate();
+
+    const handleNavigate = () => {
+        navigate("/");
+    }
+
     return (
         <>
             <h1>About</h1>
+            <button className="btn" onClick={handleNavigate}>Go to Home</button>
         </>
     )
 }
