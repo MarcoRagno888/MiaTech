68ed80d436342a257f2aeb71debdb35142541163
MarcoRagno888 <marco.cianci@5gmail.com>
2024-12-12 18:43:46 +0100

    feat: nuovo Todo

--- todo-list/src/index.ts
+++ todo-list/src/index.ts
@@ -9,3 +9,22 @@ const myTodo: Todo = {
 console.log(myTodo);
 
 let todos: Todo[] = [];
+
+function addTodo(title: string): Todo {
+    const id = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1;
+  
+    const newTodo: Todo = {
+      id,
+      title,
+      completed: false
+    };
+  
+    todos.push(newTodo);
+  
+    return newTodo;
+  }
+  
+  addTodo('Imparare TypeScript');
+  addTodo('Costruire una Todo list');
+  
+  console.log(todos);
