6b4cea1e09e2329f7d3a52047efec5aa8ea389d8
MarcoRagno888 <marco.cianci@5gmail.com>
2024-12-17 14:04:01 +0100

    feat: addTodo-User

--- todo-list/src/User.ts
+++ todo-list/src/User.ts
@@ -1,11 +1,18 @@
+import { Todo } from './types';
+
 export class User {
-    id: number;
-    name: string;
-    email?: string;
-  
-    constructor(id: number, name: string, email?: string) {
-      this.id = id;
-      this.name = name;
-      this.email = email;
-    }
+  id: number;
+  name: string;
+  email?: string;
+  todos: Todo[] = [];
+
+  constructor(id: number, name: string, email?: string) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
   }
+
+  addTodo(todo: Todo): void {
+    this.todos.push(todo);
+  }
+}
